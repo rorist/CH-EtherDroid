0c0f6f5086f2dc93f8c073defc34767044c4ac91
Aubort Jean-Baptiste <jean-baptiste.aubort@epfl.ch>
2012-02-27 17:08:31 +0100

    Basic form validation

--- src/ch/fixme/etherdroid/ActivityMain.java
+++ src/ch/fixme/etherdroid/ActivityMain.java
@@ -17,6 +17,7 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.SimpleCursorAdapter;
@@ -26,50 +27,48 @@ public class ActivityMain extends Activity {
 
     private static final int DIALOG_LOADING = 1;
     private static final int DIALOG_ADD = 2;
-    private static final int DIALOG_ERROR = 3;
     private static final String QUERY_READ = "SELECT _id,host,port from hosts";
     private static final String QUERY_ADD = "INSERT INTO hosts (host, port, apikey) values (?,?,?)";
     private SimpleCursorAdapter mAdapter;
     private Context mContext;
     private SQLiteDatabase mDb;
     private Cursor mCursor;
-	private String mErrorMessage;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
-		mContext = getApplicationContext();
-		mAdapter = new SimpleCursorAdapter(mContext, R.layout.list_host, null,
-				new String[] { "host" }, new int[] { R.id.list_title });
-		ListView list = (ListView) findViewById(R.id.list_hosts); 
-		list.setAdapter(mAdapter);
-		list.setEmptyView(findViewById(R.id.empty));
-		list.setOnItemClickListener(new OnItemClickListener() {
-			public void onItemClick(AdapterView<?> parent, View view,
-					int position, long id) {
-				// List saved pads from this host
-				Intent i = new Intent(Intent.ACTION_VIEW);
-				i.putExtra("hostid", id);
-				i.setClass(ActivityMain.this, ActivityPads.class);
-				startActivity(i);
-			}
-		});
-		
+        mContext = getApplicationContext();
+        mAdapter = new SimpleCursorAdapter(mContext, R.layout.list_host, null,
+                new String[] { "host" }, new int[] { R.id.list_title });
+        ListView list = (ListView) findViewById(R.id.list_hosts);
+        list.setAdapter(mAdapter);
+        list.setEmptyView(findViewById(R.id.empty));
+        list.setOnItemClickListener(new OnItemClickListener() {
+            public void onItemClick(AdapterView<?> parent, View view,
+                    int position, long id) {
+                // List saved pads from this host
+                Intent i = new Intent(Intent.ACTION_VIEW);
+                i.putExtra("hostid", id);
+                i.setClass(ActivityMain.this, ActivityPads.class);
+                startActivity(i);
+            }
+        });
+
         mDb = new Database(mContext).getWritableDatabase();
         new ListTask().execute();
         // FIXME: Just for dev -> open reader
-//        Intent i = new Intent(Intent.ACTION_VIEW);
-//        i.setData(Uri.parse("pad://62.220.136.218:9001/BFrMshLVWcrG4B6BsFeDRk1Iritq2Dfz/GADC2012"));
-//        startActivity(i);
-//        finish();
+        // Intent i = new Intent(Intent.ACTION_VIEW);
+        // i.setData(Uri.parse("pad://62.220.136.218:9001/BFrMshLVWcrG4B6BsFeDRk1Iritq2Dfz/GADC2012"));
+        // startActivity(i);
+        // finish();
     }
 
     @Override
     public void onDestroy() {
-		if (mCursor != null && !mCursor.isClosed()) {
-			mCursor.close();
-		}
+        if (mCursor != null && !mCursor.isClosed()) {
+            mCursor.close();
+        }
         if (mDb != null && mDb.isOpen()) {
             mDb.close();
         }
@@ -86,73 +85,83 @@ public class ActivityMain extends Activity {
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
-            case R.id.menu_add:
-                showDialog(DIALOG_ADD);
-                return true;
-            default:
-                return super.onOptionsItemSelected(item);
+        case R.id.menu_add:
+            showDialog(DIALOG_ADD);
+            return true;
+        default:
+            return super.onOptionsItemSelected(item);
         }
     }
 
     @Override
     protected Dialog onCreateDialog(int id) {
-        AlertDialog dialog = null;
+        final AlertDialog dialog;
         switch (id) {
-            case DIALOG_LOADING:
-                dialog = new ProgressDialog(this);
-                dialog.setCancelable(true);
-                dialog.setMessage("Loading...");
-                ((ProgressDialog) dialog).setIndeterminate(true);
-                break;
-            case DIALOG_ADD:
-                AlertDialog.Builder b1 = new AlertDialog.Builder(this);
-                b1.setTitle("Add host");
-                // Custom view
-				View layout = getLayoutInflater().inflate(R.layout.dialog_add_host, null);
-				b1.setView(layout);
-                // Buttons actions
-                final EditText txt_host = (EditText) layout.findViewById(R.id.add_host);
-                final EditText txt_port = (EditText) layout.findViewById(R.id.add_port);
-                final EditText txt_key = (EditText) layout.findViewById(R.id.add_apikey);
-                b1.setPositiveButton("Add", new DialogInterface.OnClickListener() {
-                    public void onClick(DialogInterface dialog, int which) {
-                    	addHost(txt_host, txt_port, txt_key);
-                    }
-                });
-                b1.setNegativeButton("Cancel", null);
-                // Create
-                dialog = b1.create();
-                break;
-            case DIALOG_ERROR:
-                AlertDialog.Builder b2 = new AlertDialog.Builder(this);
-                b2.setTitle("Error");
-                b2.setMessage(mErrorMessage);
-                b2.setNeutralButton("Ok", null);
-                dialog = b2.create();
-            	break;
+        case DIALOG_LOADING:
+            dialog = new ProgressDialog(this);
+            dialog.setCancelable(true);
+            dialog.setMessage("Loading...");
+            ((ProgressDialog) dialog).setIndeterminate(true);
+            break;
+        case DIALOG_ADD:
+            AlertDialog.Builder b1 = new AlertDialog.Builder(this);
+            b1.setTitle("Add host");
+            // Custom view
+            View layout = getLayoutInflater().inflate(R.layout.dialog_add_host,
+                    null);
+            b1.setView(layout);
+            // Buttons actions
+            final EditText txt_host = (EditText) layout.findViewById(R.id.add_host);
+            final EditText txt_port = (EditText) layout.findViewById(R.id.add_port);
+            final EditText txt_key = (EditText) layout.findViewById(R.id.add_apikey);
+            txt_host.setText("");
+            txt_port.setText("");
+            txt_key.setText("");
+            b1.setPositiveButton("Add", null);
+            b1.setNegativeButton("Cancel",
+                    new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int which) {
+                            dismissDialog(DIALOG_ADD); // FIXME: Is it working ?
+                        }
+                    });
+            // Create
+            dialog = b1.create();
+            dialog.setOnShowListener(new DialogInterface.OnShowListener() {
+                public void onShow(DialogInterface d) {
+                    Button button = dialog
+                            .getButton(DialogInterface.BUTTON_POSITIVE);
+                    button.setOnClickListener(new View.OnClickListener() {
+                        public void onClick(View v) {
+                            boolean ok = true;
+                            if (txt_host.getText().toString().length() == 0) {
+                                txt_host.setError("You must Provide an host address");
+                                ok = false;
+                            }
+                            if (txt_port.getText().toString().length() == 0) {
+                                txt_port.setError("You must Provide a port");
+                                ok = false;
+                            }
+                            if (txt_key.getText().toString().length() == 0) {
+                                txt_key.setError("You must Provide an API key");
+                                ok = false;
+                            }
+                            if (ok) {
+                                new AddHost().execute(txt_host.getText()
+                                        .toString(), txt_port.getText()
+                                        .toString(), txt_key.getText()
+                                        .toString());
+                                dialog.dismiss();
+                            }
+                        }
+                    });
+                }
+            });
+            break;
+        default:
+            dialog = null;
         }
         return dialog;
     }
-    
-	private void addHost(EditText host, EditText port, EditText key) {
-		if (new String("").equals(host.getText().toString())) {
-			mErrorMessage = "Please fill in the Host field";
-			showDialog(DIALOG_ERROR);
-			return;
-		}
-		if (new String("").equals(port.getText().toString())) {
-			mErrorMessage = "Please fill in the Port field";
-			showDialog(DIALOG_ERROR);
-			return;
-		}
-		if (new String("").equals(key.getText().toString())) {
-			mErrorMessage = "Please fill in the APIKey field";
-			showDialog(DIALOG_ERROR);
-			return;
-		}
-		new AddHost().execute(host.getText().toString(), port.getText()
-				.toString(), key.getText().toString());
-	}
 
     private class ListTask extends AsyncTask<Void, Void, Cursor> {
 
@@ -163,8 +172,8 @@ public class ActivityMain extends Activity {
 
         @Override
         protected void onPostExecute(Cursor c) {
-        	mCursor = c;
-        	mAdapter.changeCursor(mCursor);
+            mCursor = c;
+            mAdapter.changeCursor(mCursor);
         }
     }
 
@@ -180,8 +189,8 @@ public class ActivityMain extends Activity {
         protected void onPostExecute(Void unused) {
             mCursor.requery();
             mAdapter.notifyDataSetChanged();
-			Toast.makeText(mContext, "Host successfully added!",
-					Toast.LENGTH_SHORT).show();
+            Toast.makeText(mContext, "Host successfully added!",
+                    Toast.LENGTH_SHORT).show();
         }
     }
 }
